06bac933f88e41eecf7656ac4acf1932b8936c42
CodeWrangler <90991434+CodePipeWrangler@users.noreply.github.com>
2025-05-21 11:35:50 -0500

    Removed unnecessary lines

--- genomics/conv_ULTRA-tsv/ultra2tsv.sh
+++ genomics/conv_ULTRA-tsv/ultra2tsv.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# job standard output will go to the file slurm-%j.out (where %j is the job ID)
-
-#SBATCH --time=05:00:00   # walltime limit (HH:MM:SS)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=2   # 1 processor core(s) per node X 2 threads per core
-#SBATCH --partition=short    # standard node(s)
-#SBATCH --job-name="ultra2tsv"
-#SBATCH --mail-user=brandon.jordan@usda.gov   # email address
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
 for file in $1; do echo $file;
        	ref=`echo $file | perl -pe 's/(^*)\.json/$1.tsv/'` ;
        	cat $file | perl -pe 's/"//g; s/,//; s/\{//; s/\}//; s/://' | awk -v ORS="" '$1~/SequenceName/ {print "\n"} $1~/Start|Length|Period|Score|Substitutions|Insertions|Deletions|Consensus|Sequence/ {print $2 "\t"}' > $ref ; done
