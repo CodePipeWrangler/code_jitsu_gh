b53d00ceefb55846644ecb4cc4522863c154d754
Brandon Jordan <bdjordan15@gmail.com>
2025-10-24 20:07:16 -0500

    Removed outdated files

--- bin/ultra2tsv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in $1; do echo $file;
-       	ref=`echo $file | perl -pe 's/(^*)\.json/$1.tsv/'` ;
-       	cat $file | perl -pe 's/"//g; s/,//; s/\{//; s/\}//; s/://' | awk -v ORS="" '$1~/SequenceName/ {print "\n"} $1~/Start|Length|Period|Score|Substitutions|Insertions|Deletions|Consensus|Sequence/ {print $2 "\t"}' > $ref ; done
